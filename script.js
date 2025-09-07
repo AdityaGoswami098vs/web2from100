4a9fdbea2477332fa0812c6774ccf1da420ceee9
Aditya Goswami <adityagoswami098vs@gmail.com>
2025-09-07 18:49:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// For navbar
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar-menu');
+
+menu.addEventListener('click', function () {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+});
